854f0a1ab758f42f5708aa02beda665bb0262335
dylancsu <dylan.dean@csuglobal.edu>
2025-02-23 18:35:40 -0700

    fix

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@ def numToWords(num):
     return "example"
 
 def numToCheck(num):
-    return f"{numToWords(num-(num%1))} dollars and {numToWords(num%1)}/100"
+    return f"{numToWords(num-(num%1))} dollars and {num%1}/100"
 
 if __name__=="__main__":
     amount = input("enter the amount as a number: ")
