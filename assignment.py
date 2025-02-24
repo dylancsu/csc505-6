ccd008a8e65178ea9804995fa4e5dba005bf472f
dylancsu <dylan.dean@csuglobal.edu>
2025-02-23 18:34:39 -0700

    init

--- assignment.py
+++ assignment.py
@@ -1,1 +1,13 @@
+def numToWords(num):
+    return "example"
 
+def numToCheck(num):
+    return f"{numToWords(num-(num%1))} dollars and {numToWords(num%1)}/100"
+
+if __name__=="__main__":
+    amount = input("enter the amount as a number: ")
+    try:
+        num = float(input)
+        print(numToCheck(num))
+    except ValueError:
+        print("Error: not a number")
